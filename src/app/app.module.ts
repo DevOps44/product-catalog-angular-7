fbd811f8636eceae210ef194f0dd178a79be5020
DevOps44 <jmasaraure00@gmail.com>
2019-08-16 15:30:24 +0200

    Added Feature module, ProductModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
     WelcomeComponent,
-    ProductDetailComponent,
 
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    FormsModule,
     HttpClientModule,
+    ProductModule,
     RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', 
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent},
+     
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome',pathMatch: 'full'},
 
