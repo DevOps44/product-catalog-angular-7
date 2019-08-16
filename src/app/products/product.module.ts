818567e94f64525f4e615d66aed676d842d97bb4
DevOps44 <jmasaraure00@gmail.com>
2019-08-16 15:40:49 +0200

    Added Shared Module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../shared/star.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDetailComponent,
-    StarComponent,
-    ConvertToSpacesPipe
+    ProductDetailComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
+    SharedModule, 
     RouterModule.forChild([
       {path: 'products', 
         component: ProductListComponent},
@@ -27,7 +21,6 @@ import { ProductDetailGuard } from './product-detail.guard';
         component: ProductDetailComponent
       }
     ])
-
   ]
 })
 export class ProductModule { }
